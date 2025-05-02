c85a2b17808c1110db890f76d2129a56d6da8b80
Dregop <pierredrege_2@hotmail.fr>
2025-05-02 23:45:48 +0200

    remove old questionnaire

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,3 @@
 // /src/screens/index.ts
-export { default as QuestionnaireScreen } from './QuestionnaireScreen';
 export { default as ResultsScreen } from './Results/ResultsScreen';
 export { default as DetailsScreen } from './DetailsScreen';
